d311c597a56db93d9db7436d8ca54e3b6cfbe5c6
devikadasari <devikarani1182001@gmail.com>
2023-08-16 18:23:01 +0530

    petproject

---
+++ Assignment1/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models;
+using System.Collections.Generic;
+
+namespace Assignment1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee { Id = 0, Name = "EmployeeA" },
+            new Employee { Id = 1, Name = "EmployeeB" },
+            new Employee { Id = 2, Name = "EmployeeC" }
+        };
+
+        // GET: api/Employee/all
+
+        [HttpGet("all")]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(Employees);
+        }
+
+        // GET: api/Employee/byId/1
+        [HttpGet("byId/{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            if (id >= 0 && id < Employees.Count)
+            {
+                return Ok(Employees[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: api/Employee/create
+        [HttpPost("create")]
+        public IActionResult CreateEmployee([FromBody] Employee newEmployee)
+        {
+            Employees.Add(newEmployee);
+            return Created("", newEmployee);
+        }
+
+        // PUT: api/Employee/update/1
+        [HttpPut("update/{id}")]
+        public IActionResult UpdateEmployee(int id, [FromBody] Employee updatedEmployee)
+        {
+            if (id >= 0 && id < Employees.Count)
+            {
+                Employees[id] = updatedEmployee;
+                return Ok(Employees);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE: api/Employee/delete/1
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            if (id >= 0 && id < Employees.Count)
+            {
+                Employees.RemoveAt(id);
+                return Ok(Employees);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable CORS
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin(); // You can customize this to allow specific origins
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
