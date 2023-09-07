0ea1a13a53f3ccde0777c4175a21d8c55a175f45
devikadasari <devikarani1182001@gmail.com>
2023-09-07 15:29:45 +0530

    add employee changed in frontend

--- frontend/src/components/EmployeePost.js
+++ frontend/src/components/EmployeePost.js
@@ -2,36 +2,50 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../styles/Add.css';
 
-function EmployeePost({token}) {
+function EmployeePost({ token }) {
   const [newEmployee, setNewEmployee] = useState({
     empName: '',
-    
+    department: {
+      departmentName: '',
+    },
+    employeeAddress: '',
+    employeeSalary: '',
   });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setNewEmployee((prevEmployee) => ({
-      ...prevEmployee,
-      [name]: value,
-    }));
+    if (name.startsWith('department.')) {
+      setNewEmployee((prevEmployee) => ({
+        ...prevEmployee,
+        department: {
+          ...prevEmployee.department,
+          [name.replace('department.', '')]: value,
+        },
+      }));
+    } else {
+      setNewEmployee((prevEmployee) => ({
+        ...prevEmployee,
+        [name]: value,
+      }));
+    }
   };
 
   const handleCreateEmployee = async () => {
     try {
-      // Send a POST request to create the employee
       await axios.post('https://localhost:7277/api/Employee/create', newEmployee, {
         headers: {
           Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
         },
       });
-      
-     
-     // fetchEmployees();
-      
-      
+
       setNewEmployee({
         empName: '',
-       
+        department: {
+          departmentName: '',
+        },
+        employeeAddress: '',
+        employeeSalary: '',
       });
     } catch (error) {
       console.error('Error creating employee:', error);
@@ -40,20 +54,51 @@ function EmployeePost({token}) {
 
   return (
     <div className="employee-post-container">
-      <h2>Create New Employee</h2>
-      <div className="post-form">
-        <input
-          type="text"
-          className='add-input'
-          placeholder="Employee Name"
-          name="empName"
-          value={newEmployee.empName}
-          onChange={handleInputChange}
-        />
-       
-        <button className="post-button" onClick={handleCreateEmployee}>
-          Create
-        </button>
+      
+      <div className="form-container">
+      
+        <div className="post-form">
+        <h2>Create New Employee</h2>
+          <input
+            type="text"
+            className="add-input"
+            placeholder="Employee Name"
+            name="empName"
+            value={newEmployee.empName}
+            onChange={handleInputChange}
+          />
+          <br />
+          <input
+            type="text"
+            className="add-input"
+            placeholder="Department Name"
+            name="department.departmentName"
+            value={newEmployee.department.departmentName}
+            onChange={handleInputChange}
+          />
+          <br />
+          <input
+            type="text"
+            className="add-input"
+            placeholder="Employee Address"
+            name="employeeAddress"
+            value={newEmployee.employeeAddress}
+            onChange={handleInputChange}
+          />
+          <br />
+          <input
+            type="text"
+            className="add-input"
+            placeholder="Employee Salary"
+            name="employeeSalary"
+            value={newEmployee.employeeSalary}
+            onChange={handleInputChange}
+          />
+          <br />
+          <button className="post-button" onClick={handleCreateEmployee}>
+            Create
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import '../styles/Header.css'; // Import the CSS file for styling
 function Header() {
   return (
     <div className="header-container">
-      <div className="logo">Employee API</div>
+      <div className="logo">Employee Management </div>
       <div className="nav-links">
         <Link to="/EmployeeList">Search Employees</Link>
         <Link to="/EmployeePost">Add Employee</Link>
